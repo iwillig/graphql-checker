03877a0a5c49c7a7b3f7e7fde34d4711f1926172
Ivan Willig <iwillig@gmail.com>
2024-02-05 20:53:37 -0500

    Do some stuff

--- src/graphql_checker/main.clj
+++ src/graphql_checker/main.clj
@@ -5,13 +5,120 @@
    ;;[clj-commons.format.exceptions :as exceptions]
    ;;[clojure.stacktrace :as stacktrace]
    [com.walmartlabs.lacinia.parser.common :as common]
-   ;;[com.walmartlabs.lacinia.parser.schema :as schema]
+   ;; [com.walmartlabs.lacinia.parser.schema :as schema]
    ;;[puget.printer :as puget]
 
    [clojure.java.io :as io]))
 
 ;; Load Lacinia's GraphQL Schema
-(def grammar (common/compile-grammar "com/walmartlabs/lacinia/schema.g4"))
+(def grammar (common/compile-grammar
+              "com/walmartlabs/lacinia/schema.g4"))
+
+(defrecord NameToken [name-value])
+(defrecord TypeSpec  [type-name requied])
+(defrecord FieldDef  [field-name type-spec])
+(defrecord TypeDef   [type-name fields])
+(defrecord ListType  [type-sec])
+
+
+(comment
+
+  :graphqlSchema
+
+  :typeDef
+  :typeSpec
+  :fieldDef
+
+
+  :anyName
+  :nameTokens
+
+
+  )
+
+;; Taken and simplifed from Lacina parser.schema
+(defmulti xform
+  first)
+
+(defmethod xform :default [prod]
+  prod)
+
+(defn maybe-xform
+  [x]
+  (cond (string? x) x
+        (seqable? x)
+        (xform x)
+        :else x))
+
+(defmethod xform :nameTokens
+  [[_ token]]
+  token)
+
+(defmethod xform :anyName
+  [[_ name-tokens]]
+  (xform name-tokens))
+
+(comment
+  '(:implementationDef implements Sentient))
+
+(defmethod xform :implementationDef
+  [[_ operation & types]]
+  [(keyword operation)
+   (remove #{"&"} types)])
+
+
+(comment
+  '(:fieldDef
+    (:anyName (:nameTokens "name"))
+    ":"
+    (:typeSpec
+     (:typeName (:anyName (:nameTokens "String")))
+     (:required !))))
+
+(comment
+  (:typeSpec
+   (:typeName (:anyName (:nameTokens "String")))
+   (:required "!")))
+
+(def drop-string-xform
+  (comp
+   (remove string?)
+   (map xform)
+   (remove nil?)))
+
+(defmethod xform :typeSpec
+  [[_ type-name require]]
+  [(xform type-name) (xform require)])
+
+(defmethod xform :fieldDef
+  [[_ field-name _ type-spec]]
+  [(xform field-name) (xform type-spec)])
+
+(defmethod xform :typeName
+  [[_ any-name]]
+  (xform any-name))
+
+(defmethod xform :fieldDefs
+  [[_ & rest-prod]]
+  (into []
+        drop-string-xform
+        rest-prod))
+
+(defmethod xform :listType
+  [[_ & rest-prod]]
+  [:listType
+   (into []
+         drop-string-xform
+         rest-prod)])
+
+(defmethod xform :typeDef
+  [prod]
+  (let [type-info (nthrest prod 2)]
+    [:typeDef
+     (map
+      maybe-xform
+      type-info)]))
+
 
 (defn parse-schema
   [path]
@@ -19,5 +126,12 @@
         parsed-schema (antlr/parse grammar {:throw? false} gql-string)]
     parsed-schema))
 
+(defn- xform-schema
+  [schema-tree]
+  (->> schema-tree
+       (rest)
+       (map xform)))
+
 (defn -main [& _args]
-  (parse-schema "test-data/example.graphql"))
+  (xform-schema
+   (parse-schema "test-data/example.graphql")))
--- test/graphql_checker/main_test.clj
+++ test/graphql_checker/main_test.clj
@@ -18,5 +18,42 @@
 (t/deftest test-parse-schema
   (t/testing "when we parse the schema"
     (let [thing (gqlc.main/-main)]
-      (t/is (match? nil?
-                    thing)))))
+
+      (binding [*print-meta* false]
+
+        (t/is (match? nil?
+                      thing))))))
+
+
+(t/deftest test-xform
+
+  (t/testing "When given a valid type def"
+    (t/is (match?
+           nil?
+           (gqlc.main/xform
+            '(:typeDef
+              "type"
+              (:anyName (:nameTokens "Human"))
+              (:implementationDef "implements" "Pet" "&" "NickName")
+              (:fieldDefs
+               "{"
+               (:fieldDef
+                (:anyName (:nameTokens "name"))
+                ":"
+                (:typeSpec
+                 (:typeName (:anyName (:nameTokens "String")))
+                 (:required "!")))
+               (:fieldDef
+                (:anyName (:nameTokens "pets"))
+                ":"
+                (:typeSpec
+                 (:listType
+                  "["
+                  (:typeSpec
+                   (:typeName (:anyName (:nameTokens "Pet")))
+                   (:required "!"))
+                  "]")))
+               "}")))
+           ))
+
+    ))
