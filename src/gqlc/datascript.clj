4645dc8d17db157e545e48c135021ea076580a65
Ivan Willig <iwillig@gmail.com>
2024-02-25 10:33:38 -0500

    Start defining core entity types

--- src/gqlc/datascript.clj
+++ src/gqlc/datascript.clj
@@ -33,8 +33,31 @@
 
   )
 
+(comment
+  {:row    (dec (.getLine start-token))
+   :column (.getCharPositionInLine start-token)
+   :index  (.getStartIndex start-token)
+   :stop   (.getStopIndex stop-token)})
 
-;; :db/cardinality :db.cardinality/many
+
+;; Clojure/DataScript core entity types
+;; --------------------------------------------------
+
+;; position [DONE]
+;; description [TODO]
+;; name-token [TODO]
+;; list-name-token [TODO]
+;; type-spec [TODO]
+;; field-def [TODO]
+;; fields  [TODO]
+;; list-type [TODO]
+;; type-def
+;; argument
+;; arg-list
+;; implements
+;; required
+;; graphql-schema
+
 
 (def db-schema
   "Datascript schema.
@@ -44,18 +67,43 @@
   {
    ;; Position
    :position/row
-   {}
+   {:db/doc
+    "The line number or 'row' of the expression.
+     Implemented in clj-antrl as `(dec (.getLine start-token))`"}
+
    :position/column
-   {}
+   {:db/doc
+    "The column of the expression
+     Implemented in clj-antrl as `(.getCharPositionInLine start-token)`"}
+
    :position/index
-   {}
+   {:db/doc ""}
+
    :position/stop
-   {}
+   {:db/doc "The end of the GraphQL Expression"}
 
    ;; Form
-   :form/position {:db/valueType :db.type/ref}
+   :form/position
+   {:db/valueType :db.type/ref
+    :db/doc ""}
 
    ;; Type Description
-   :description/value {}
+   :description/value
+   {:db/doc ""}
 
    })
+
+
+
+
+
+(defn create-schema-db
+  "Given no args
+   Returns a new datascript connection object"
+  []
+  (d/create-conn db-schema))
+
+(comment
+  (defonce db (create-schema-db)))
+
+(def db (create-schema-db))
